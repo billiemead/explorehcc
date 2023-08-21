93fbf544165b404599461d9e6bca5018ed59f1d4
billiemead <bmead@mjhlifesciences.com>
2023-08-21 09:16:43 -0400

    fixed bottom footer update - all pages

--- wp-content/themes/conall/framework/modules/footer/templates/parts/content-bottom-area.php
+++ wp-content/themes/conall/framework/modules/footer/templates/parts/content-bottom-area.php
@@ -1,5 +1,5 @@
 <?php if(($content_bottom_area == "yes") && (is_active_sidebar($content_bottom_area_sidebar))) { ?>
-	<div class="edgtf-content-bottom" <?php conall_edge_inline_style($content_bottom_style); ?>>
+	<div class="edgtf-content-bottom" >
 		<?php if($content_bottom_area_in_grid == 'yes'){ ?>
 			<div class="edgtf-container">
 				<div class="edgtf-container-inner clearfix">
