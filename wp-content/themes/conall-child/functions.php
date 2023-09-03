f62b7a95fa688b6b5b05acdc501e1c1591bb5ac3
billiemead <billie@billiemead.com>
2023-09-02 20:49:54 -0400

    move video to seperate script

--- wp-content/themes/conall-child/functions.php
+++ wp-content/themes/conall-child/functions.php
@@ -18,8 +18,11 @@ add_action('wp_enqueue_scripts', 'conall_edge_child_theme_enqueue_scripts');
 
 function elevar_assets()
 {
-    wp_register_style('elevar-stylesheet', get_theme_file_uri() . '/dist/css/bundle.css', array(), '1.0.0', 'all');
-    wp_enqueue_style('elevar-stylesheet');
-    wp_enqueue_script('elevar_js', get_theme_file_uri() . '/dist/js/bundle.js', array('jquery'), '1.0.0', true);
+    if (is_home() || is_front_page()) {
+        wp_enqueue_script('elevar_movie', get_theme_file_uri() . '/movie.js', '1.0.0', true);
+    }
+        wp_register_style('elevar-stylesheet', get_theme_file_uri() . '/dist/css/bundle.css', array(), '1.0.0', 'all');
+        wp_enqueue_style('elevar-stylesheet');
+        wp_enqueue_script('elevar_js', get_theme_file_uri() . '/dist/js/bundle.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'elevar_assets');
---
+++ wp-content/themes/conall-child/movie.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+    //write your code here
+    function toggleMute() {
+        var button = document.getElementById("toptab-link3")
+        var video = document.getElementById("elevarVideo")
+
+        if (video.muted) {
+            video.muted = false;
+        } else {
+            video.muted = true;
+        }
+        button.classList.toggle('muted');
+    }
+
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+        // create the video element
+        var video = document.createElement("video");
+        video.setAttribute("data-v-767e84ec", "");
+        video.setAttribute("webkit-playsinline", "");
+        video.setAttribute("playsinline", "");
+        video.setAttribute("poster", "/wp-content/uploads/videos/hero-video-poster-image1920x900.jpg");
+        /* video.setAttribute("muted", "muted"); */
+        video.setAttribute("loop", "loop");
+        video.setAttribute("src", "/wp-content/uploads/videos/elevar_da_mountain_animation_1920x900.mp4");
+        video.setAttribute("width", "100vw");
+        video.setAttribute("height", "100vh");
+        video.setAttribute("class", "elevar-animation");
+        video.setAttribute("id", "elevarVideo");
+        video.setAttribute("autoplay", "autoplay");
+        // get the existing video container and append the new video element
+        var videoContainer = document.querySelector('.video-wrap');
+        videoContainer.appendChild(video);
+    }
+}
