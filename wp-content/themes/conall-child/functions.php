9acf86a2b1229b5065983e6f7882f818b34493a4
Billie Mead <bmead@mjhlifesciences.com>
2024-03-05 15:25:36 -0500

    Add Sarah & Greg video to tommorrow page

--- wp-content/themes/conall-child/functions.php
+++ wp-content/themes/conall-child/functions.php
@@ -64,3 +64,20 @@ function hubspot_javascript()
     <?php
 }
 add_action('wp_head', 'hubspot_javascript');
+
+// Add marker.io script to page head
+function marker_io()
+{
+    ?>
+      <script>
+        window.markerConfig = {
+            project: '65e77e8458da5533cb245ddd',
+            source: 'snippet'
+        };
+
+        !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
+        </script>
+
+    <?php
+}
+add_action('wp_head', 'marker_io');
